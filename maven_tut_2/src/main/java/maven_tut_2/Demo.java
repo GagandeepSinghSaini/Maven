4ccebe856156eee7bcc633c1a6abb128c1b0a9ee
GagandeepSinghSaini <g.saini2692@gmail.com>
2017-04-25 01:02:39 +0530

    First Maven

---
+++ maven_tut_2/src/main/java/maven_tut_2/Demo.java
@@ -0,0 +1,8 @@
+package maven_tut_2;
+
+public class Demo {
+
+	public String showMsg() {
+		return "HELLO";
+	}
+}
---
+++ maven_tut_2/src/test/java/maven_tut_2/DemoTest.java
@@ -0,0 +1,15 @@
+package maven_tut_2;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class DemoTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
